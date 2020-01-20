295bc9d458d82b115ab5ab096143e26a6e1e6830
Rafael Colmanetti <rafaelccolmanetti@gmail.com>
2020-01-20 15:47:29 +0100

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// $(".toggle1").slideUp();
+// $(".trigger1").click(function(){
+// 	$(this).next(".toggle1").slideToogle("slow");
+// });
+
+// $(".toggle2").slideUp();
+// $(".trigger2").click(function(){
+// 	$(this).next(".toggle2").slideToogle("slow");
+// });
+
